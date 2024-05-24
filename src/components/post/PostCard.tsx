fd73cb89b6731e21d5774a4f3e6bce6622e43e6f
mmoc0910 <mmoc0910@gmail.com>
2024-05-24 22:41:27 +0700

    reherh

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,23 +1,27 @@
 import React, { FC } from "react";
 import { PostType } from "./type";
 import { Link } from "react-router-dom";
+import { getRandomImage, isHttpsImageUrl } from "../../constants";
 
 type PostCardProps = { post: PostType };
 const PostCard: FC<PostCardProps> = ({ post }) => {
   const { description, href, thumbnail, title } = post;
-  return (
-    <Link className="block" to={`/post?href=${href}&img=[${thumbnail}]`}>
-      <img
-        src={thumbnail}
-        alt={title}
-        className="w-full aspect-[16/10] object-cover rounded-md"
-      />
-      <div className="pt-3">
-        <h4 className="font-semibold text-lg line-clamp-2 mb-1">{title}</h4>
-        <p className="line-clamp-3 text-gray-500">{description}</p>
-      </div>
-    </Link>
-  );
+  const image = isHttpsImageUrl(thumbnail) ? thumbnail : getRandomImage();
+  if (title)
+    return (
+      <Link className="block" to={`/post?href=${href}&img=[${image}]`}>
+        <img
+          src={image}
+          alt={title}
+          className="w-full aspect-[16/10] object-cover rounded-md"
+        />
+        <div className="pt-3">
+          <h4 className="font-semibold text-lg line-clamp-2 mb-1">{title}</h4>
+          <p className="line-clamp-3 text-gray-500">{description}</p>
+        </div>
+      </Link>
+    );
+  return;
 };
 
 export default PostCard;
---
+++ src/constants/index.ts
@@ -0,0 +1,27 @@
+export const images = [
+  "https://images.unsplash.com/photo-1716436329478-e4261139e1ca?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1715693352133-ffa8caa4e7e8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1715671525805-ed50afea6d13?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1716213420631-286d1d6360f5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1714969894946-8d5e55419ed3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1716377239833-54b55d732bc8?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1715276611636-ca01632a691a?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1716091975692-d40296327f4a?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1716303829458-8b7469ac20a3?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1714997219788-660af304f464?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1714837003223-5144b6e082cb?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+export function getRandomImage() {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  return images[randomIndex];
+}
+
+export function isHttpsImageUrl(url: string) {
+  try {
+    const parsedUrl = new URL(url);
+    return parsedUrl.protocol === "https:";
+  } catch (e) {
+    return false; // Nếu URL không hợp lệ, return false
+  }
+}
